0a79fc94bdf977bee788a93b4a40dd61d3a862f5
Andrea-Lulli <andrea.lulli89@gmail.com>
2023-03-28 17:45:29 +0200

    show

--- resources/views/admin/perfumes/show.blade.php
+++ resources/views/admin/perfumes/show.blade.php
@@ -1,10 +1,31 @@
+
 @extends('layouts.app')
 @section('content')
-<div class="text-center my-5">
-    <h2>{{ $singolo_perfume->name }}</h2>
-    <p>Marca: {{ $singolo_perfume->brand }}</p>
-    <p>Prezzo: {{ $singolo_perfume->price }}€</p>
-    <p>Categoria: {{ $singolo_perfume->category }}</p>
-    <img class="w-25" src="{{$singolo_perfume->image}}" alt="">
-</div>
+    <div>
+        <div id="user_index" class="row flex-column align-items-center  mb-5">
+            <h1 class="col-12 text-center p-3 mb-5 shadow">
+                DETTAGLI PRODOTTO
+            </h1>
+                    <div class="card m-3 shadow w-75 ">
+                        <div class="card-body row">
+                            <div class="col-md-12">
+                                <h4>{{ $singolo_perfume->name }}</h4>
+                                 <hr>
+                                <h5>Marca: {{ $singolo_perfume->brand }}</h5>
+                                <p>Prezzo: {{ $singolo_perfume->price }}€</p>
+                                <hr>
+                                <p>Categoria: {{ $singolo_perfume->category }}</p>
+                                <p>Prezzo: 5.00€</p>
+                                <hr>
+                                <img class="w-25" src="{{$singolo_perfume->image}}" alt="">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-12 d-flex justify-content-center">
+                        <div >
+                            <a  class="btn btn-outline-danger" href="{{ route('admin.perfumes.index') }}" >Indietro</a>
+                        </div>
+                    </div>
+
+    </div>
 @endsection
